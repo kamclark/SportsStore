872f063fa1d6187178e4d1c2e5f15bdf994f1cbc
Kamari Clark <kamnmclark@gmail.com>
2017-10-25 16:05:37 -0400

    Initital commit.

---
+++ SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+
+    public interface IOrderRepository
+    {
+
+        IEnumerable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
